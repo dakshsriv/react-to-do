2d3e397ac6f84c1464a637f15ac3018e796e9a32
Daksh Srivastava <daksh.srivastava@gmail.com>
2021-12-05 15:53:51 -0500

    Create feature finished

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -35409,97 +35409,69 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+var Create = function Create(props) {
+  var _useState = (0, _react.useState)(""),
+      _useState2 = _slicedToArray(_useState, 2),
+      newEntry = _useState2[0],
+      setNewEntry = _useState2[1];
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+  var updateNewEntry = function updateNewEntry(event) {
+    console.log("updateNewEntry has been triggered");
+    console.log('event.target.value', event.target.value);
+    setNewEntry({
+      newEntry: event.target.value
+    });
+  };
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+  var onTrigger = function onTrigger(event) {
+    console.log(newEntry.newEntry);
+    props.parentCallback(newEntry.newEntry);
+  };
 
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+  var clearEntry = function clearEntry() {
+    console.log(newEntry);
+    console.log("Clearing");
+    console.log(newEntry);
+    setNewEntry("");
+  };
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+  var cancelEntry = function cancelEntry() {
+    props.parentCallback("");
+  };
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var Create = /*#__PURE__*/function (_Component) {
-  _inherits(Create, _Component);
-
-  var _super = _createSuper(Create);
-
-  function Create() {
-    var _this;
-
-    _classCallCheck(this, Create);
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "Create a note here"), /*#__PURE__*/_react.default.createElement("hr", null), /*#__PURE__*/_react.default.createElement("form", {
+    onSubmit: onTrigger,
+    onReset: clearEntry
+  }, /*#__PURE__*/_react.default.createElement("label", null, /*#__PURE__*/_react.default.createElement("textarea", {
+    rows: 7,
+    cols: 90,
+    onChange: updateNewEntry
+  }, "Enter your note here")), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("input", {
+    type: "submit",
+    value: "Submit"
+  }), /*#__PURE__*/_react.default.createElement("div", {
+    style: {
+      display: 'inline-block'
     }
-
-    _this = _super.call.apply(_super, [this].concat(args));
-
-    _defineProperty(_assertThisInitialized(_this), "state", {
-      newEntry: ""
-    });
-
-    _defineProperty(_assertThisInitialized(_this), "updateNewEntry", function (event) {
-      console.log("updateNewEntry has been triggered");
-      console.log('event.target.value', event.target.value);
-
-      _this.setState({
-        newEntry: event.target.value
-      });
-    });
-
-    _defineProperty(_assertThisInitialized(_this), "onTrigger", function (event) {
-      console.log(_this.state.newEntry);
-
-      _this.props.parentCallback(_this.state.newEntry);
-    });
-
-    _defineProperty(_assertThisInitialized(_this), "cancelEntry", function () {
-      _this.props.parentCallback("");
-    });
-
-    return _this;
-  }
-
-  _createClass(Create, [{
-    key: "render",
-    value: function render() {
-      return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "Create a note here"), /*#__PURE__*/_react.default.createElement("hr", null), /*#__PURE__*/_react.default.createElement("form", {
-        onSubmit: this.onTrigger
-      }, /*#__PURE__*/_react.default.createElement("label", null, /*#__PURE__*/_react.default.createElement("textarea", {
-        rows: 7,
-        cols: 90,
-        value: this.state.newEntry,
-        onChange: this.updateNewEntry
-      }, "Enter your note here")), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("input", {
-        type: "submit",
-        value: "Submit"
-      }), /*#__PURE__*/_react.default.createElement("div", {
-        style: {
-          display: 'inline-block'
-        }
-      }, /*#__PURE__*/_react.default.createElement("button", {
-        onClick: this.cancelEntry
-      }, "Cancel")))));
-    }
-  }]);
-
-  return Create;
-}(_react.Component);
+  }, /*#__PURE__*/_react.default.createElement("input", {
+    type: "reset",
+    value: "Clear"
+  }), /*#__PURE__*/_react.default.createElement("button", {
+    onClick: cancelEntry
+  }, "Cancel")))));
+};
 
 var _default = Create;
 exports.default = _default;
@@ -36382,7 +36354,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41499" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41883" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,46 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class Create extends Component {
-    state = { newEntry: ""};
+const Create = props => {
+    const [newEntry, setNewEntry] = useState("");
 
-    updateNewEntry = event => {
+    const updateNewEntry = event => {
         console.log("updateNewEntry has been triggered");
         console.log('event.target.value', event.target.value);
-        this.setState({newEntry: event.target.value});
+        setNewEntry({newEntry: event.target.value});
     }
 
-    onTrigger = (event) => {
-        console.log(this.state.newEntry);
-        this.props.parentCallback(this.state.newEntry);
+    const onTrigger = (event) => {
+        console.log(newEntry.newEntry);
+        props.parentCallback(newEntry.newEntry);
         }
 
-    cancelEntry = () => {
-        this.props.parentCallback("");
+    const clearEntry = () => {
+        console.log(newEntry);
+        console.log("Clearing");
+        console.log(newEntry);
+        setNewEntry("");
     }
 
-    render() {
-        return (
-            <div>
-                <h1>Create a note here</h1>
-                <hr />
-                <form onSubmit={this.onTrigger}>
-                    <label>
-                        <textarea rows={7} cols={90} value={this.state.newEntry} onChange={this.updateNewEntry}>
-                        Enter your note here
-                        </textarea>
-                    </label>
-                    <div>
-                        <input type="submit" value="Submit" />
-                        <div style={{display: 'inline-block'}}>
-                            <button onClick={this.cancelEntry}>Cancel</button>
-                        </div>
-                    </div>
-                </form>
-                
-            </div>
-        )
+    const cancelEntry = () => {
+        props.parentCallback("");
     }
+
+    return (
+        <div>
+            <h1>Create a note here</h1>
+            <hr />
+            <form onSubmit={onTrigger} onReset={clearEntry}>
+                <label>
+                    <textarea rows={7} cols={90}  onChange={updateNewEntry}>
+                    Enter your note here
+                    </textarea>
+                </label>
+                <div>
+                    <input type="submit" value="Submit" />
+                    <div style={{display: 'inline-block'}}>
+                    <input type="reset" value="Clear"/>
+                    <button onClick={cancelEntry}>Cancel</button>
+                    </div>
+                </div>
+            </form>
+            
+        </div>
+    )
 }
 
 export default Create;
